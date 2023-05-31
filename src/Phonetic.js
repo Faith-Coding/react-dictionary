940049790d7bb1a88941e6b2d270760b9d0bebe5
Faith-Coding <Akhigbefaithbright@gmail.com>
2023-06-01 01:10:44 +0200

    Added photos file

---
+++ src/Phonetic.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { GiSpeaker } from "react-icons/gi";
+import "./Phonetic.css";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <GiSpeaker className="speaker" />
+      </a>
+      <span className="text">{props.phonetic.text}</span>
+    </div>
+  );
+}
